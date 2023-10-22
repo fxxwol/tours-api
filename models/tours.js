62243e2d699d2749289e021ad2a31668f47002ee
fxxwol <protsivroksolana2004@gmail.com>
2023-10-22 04:46:32 +0300

    initial

---
+++ models/tours.js
@@ -0,0 +1,11 @@
+const getAllTours = async () => {}
+
+const getTourById = async (tourId) => {}
+
+const getTourByName = async (tourName) => {}
+
+module.exports = {
+  getAllTours,
+  getTourById,
+  getTourByName,
+}
